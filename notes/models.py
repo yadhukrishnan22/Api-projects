ecf6be17b1d6a577c7e15f649f39eb0bcec23358
sajay <sajaykannan10@gmail.com>
2024-09-24 13:07:04 +0530

    latest

---
+++ notes/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Task(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    description=models.TextField()
+
+    status_choices=(
+        ("pending","pending"),
+        ("in-progress","in-progress"),
+        ("done","done")
+    )
+
+    status=models.CharField(max_length=200,choices=status_choices,
+                            default="pending"
+                            )
+    created_date=models.DateTimeField(auto_now_add=True)
+
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    category_choices=(
+        ("business","business"),
+        ("personal","personal")
+    )
+
+    category=models.CharField(max_length=200,choices=category_choices,
+                            default="personal")
+    
+    priority_choices=(
+        ("low","low"),
+        ("medium","medium"),
+        ("high","high")
+    )
+
+    priority=models.CharField(max_length=200,choices=priority_choices,
+                              default="low")
+    
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+
+        return self.title
